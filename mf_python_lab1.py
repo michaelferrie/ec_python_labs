eb3f1e73462753c8ce4749c5f1c9c6ee61402abd
michaelferrie <me@michaelferrie.com>
2019-05-25 14:33:44 +0100

    Create mf_python_lab1.py

---
+++ mf_python_lab1.py
@@ -0,0 +1,59 @@
+#
+# Edinburgh College
+# Lab 1 / Computational Thinking
+# Student Name: 
+#
+# Printing is easy, all you do is type print, then put what you want to print in doube quotes and parenthesis -> print ("like this")
+
+print ("Question 1: ")
+# leave only the correct answer
+print ("What is the difference between an Algorithm and a Program?")
+print ("An algorithm is a conceptual idea, a program is a concrete instantiation of an algorithm.")
+print ("An algorithm is limited to mathematical operation, a program can specify all kinds of operations.")
+print ("An algorithm makes a slow program run fast.")
+print ("An algorithm deals with computer hardware, a program deals with computer software.")
+
+print ("Question 2: ")
+print ("True or False? A computational mode of thinking means that everything can be viewed as a math problem involving numbers and formulas.")
+# write the print statement here to print true or false to the screen.
+
+print ("Question 3: ")
+print (" True or False? Computer Science is the study of how to build efficient machines that run programs.")
+# write the print statement here to print true or false to the screen.
+
+print ("Question 4: ")
+print ("The two things every computer can do are: ")
+# leave only the correct two answers
+print ("Perform calculations, Convert electricity to numbers, Display results to a screen, Remember the results")
+
+print ("Question 5: ")
+print ("True or False? Declarative knowledge refers to statements of fact.")
+# write the print statement here to print true or false to the screen.
+
+print ("Question 6: ")
+print ("Imperative knowledge refers to 'how to' methods.")
+# write the print statement here to print true or false to the screen.
+
+print ("Question 7: ")
+print ("True or False? A recipe for deducing the square root involves guessing a starting value for y. Without another recipe to be told how to pick a starting number, the computer cannot generate one on its own.")
+# write the print statement here to print true or false to the screen.
+
+print ("Question 8: ")
+print ("Ascii art time, observe the following pattern of print statements, can you use the technique below to print an uppercase letter E")
+# the letter 'A'
+print("    *")
+print("   * *")
+print("  *****")
+print(" *     *")
+print("*       *")
+print()
+
+print ("Question 9: ")
+print ("Use the characters ^ and ) to print out a smiley face, with a minimum of 6 print statements - make it big if you are an artist")
+
+print ("Question 10: ")
+# This question is more of a checklist, print out yes when completed, you can say no to liking python, as long as you do the work (-:
+print ("I have completed all of the questions here...")
+print ("I have created a Github account")
+print ("I have uploaded this script to Github and named it lab1_week1.py ")
+print ("I like python")
